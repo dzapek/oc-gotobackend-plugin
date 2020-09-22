07cf3c46be7bedb3e0e4d15c131653cc7dba7377
dzapek <patryk@trad.dev>
2020-09-22 20:51:01 +0200

    add js

--- Plugin.php
+++ Plugin.php
@@ -2,20 +2,37 @@
 
 namespace Trad\Gotobackend;
 
+use Block;
 use Backend;
 use System\Classes\PluginBase;
+use System\Classes\PluginManager;
 use Illuminate\Support\Facades\Event;
 
-/**
- * gotobackend Plugin Information File
- */
 class Plugin extends PluginBase
 {
     public function boot()
     {
-        Event::listen('cms.page.beforeDisplay', function($controller, $action, $params) {
-            $controller->addCss($this->pathToPlugin . '/assets/gotobackend.css');
+        Event::listen('cms.page.beforeDisplay', function ($controller) {
+            $pluginManager = PluginManager::instance();
+            $pluginPath = $pluginManager->getPluginPath('Trad.Gotobackend');
+            $controller->addCss($pluginPath . '/assets/gotobackend.css');
+            $this->initJs();
         });
+    }
+
+    private function initJs()
+    {
+        $pathBackend = Backend::url();
+
+        $initJS = <<<JS
+            var linkBackend = $('<a>',{
+                text: 'goToBackend',
+                title: 'Go to Backend',
+                href: '{$pathBackend}',
+                class: 'gotobackend'
+            }).appendTo('body');
+        JS;
+        Block::append('scripts', '<script>' . $initJS . '</script>');
     }
 
     public function registerPermissions()
