01214e5d79294c61dd61804885a3d06b428aafab
dzapek <patryk@trad.dev>
2020-09-22 10:05:59 +0200

    add css

--- Plugin.php
+++ Plugin.php
@@ -1,67 +1,23 @@
-<?php namespace Trad\Gotobackend;
+<?php
+
+namespace Trad\Gotobackend;
 
 use Backend;
 use System\Classes\PluginBase;
+use Illuminate\Support\Facades\Event;
 
 /**
  * gotobackend Plugin Information File
  */
 class Plugin extends PluginBase
 {
-    /**
-     * Returns information about this plugin.
-     *
-     * @return array
-     */
-    public function pluginDetails()
-    {
-        return [
-            'name'        => 'gotobackend',
-            'description' => 'No description provided yet...',
-            'author'      => 'trad',
-            'icon'        => 'icon-leaf'
-        ];
-    }
-
-    /**
-     * Register method, called when the plugin is first registered.
-     *
-     * @return void
-     */
-    public function register()
-    {
-
-    }
-
-    /**
-     * Boot method, called right before the request route.
-     *
-     * @return array
-     */
     public function boot()
     {
-
+        Event::listen('cms.page.beforeDisplay', function($controller, $action, $params) {
+            $controller->addCss($this->pathToPlugin . '/assets/gotobackend.css');
+        });
     }
 
-    /**
-     * Registers any front-end components implemented in this plugin.
-     *
-     * @return array
-     */
-    public function registerComponents()
-    {
-        return []; // Remove this line to activate
-
-        return [
-            'Trad\Gotobackend\Components\MyComponent' => 'myComponent',
-        ];
-    }
-
-    /**
-     * Registers any back-end permissions used by this plugin.
-     *
-     * @return array
-     */
     public function registerPermissions()
     {
         return []; // Remove this line to activate
@@ -70,26 +26,6 @@ class Plugin extends PluginBase
             'trad.gotobackend.some_permission' => [
                 'tab' => 'gotobackend',
                 'label' => 'Some permission'
-            ],
-        ];
-    }
-
-    /**
-     * Registers back-end navigation items for this plugin.
-     *
-     * @return array
-     */
-    public function registerNavigation()
-    {
-        return []; // Remove this line to activate
-
-        return [
-            'gotobackend' => [
-                'label'       => 'gotobackend',
-                'url'         => Backend::url('trad/gotobackend/mycontroller'),
-                'icon'        => 'icon-leaf',
-                'permissions' => ['trad.gotobackend.*'],
-                'order'       => 500,
             ],
         ];
     }
---
+++ lang/en/lang.php
@@ -0,0 +1,6 @@
+<?php return [
+    'plugin' => [
+        'name' => 'GoToBackend',
+        'description' => 'Plugin that adds a button leading to the backend on the frontend',
+    ],
+];
